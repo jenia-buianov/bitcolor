907e3097549aa41a09f256fded783ed20e4a894b
jenia-buianov <jeniabuianov@gmail.com>
2017-02-20 13:01:37 +0200

    Changed database access

--- config/database.php
+++ config/database.php
@@ -54,11 +54,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => env('DB_HOST', '31.22.4.254'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'bitcolor'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'buianove_color'),
+            'username' => env('DB_USERNAME', 'buianove_color'),
+            'password' => env('DB_PASSWORD', 'ABCD0781946210'),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
